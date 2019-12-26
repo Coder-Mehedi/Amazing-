e34ea3389f7f96ad6bf31e9ff80fa0884a9ea03c
Coder-Mehedi <mehedi5051@gmail.com>
2019-12-26 18:13:00 +0600

    banner section done

--- functions.php
+++ functions.php
@@ -1,45 +1,12 @@
 <?php
-/**
- * amazing functions and definitions
- *
- * @link https://developer.wordpress.org/themes/basics/theme-functions/
- *
- * @package amazing
- */
 
 if ( ! function_exists( 'amazing_setup' ) ) :
-	/**
-	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
-	 */
+
 	function amazing_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on amazing, use a find and replace
-		 * to change 'amazing' to the name of your theme in all the template files.
-		 */
+		
 		load_theme_textdomain( 'amazing', get_template_directory() . '/languages' );
-
-		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
 		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
 		add_theme_support( 'post-thumbnails' );
 
 		// This theme uses wp_nav_menu() in one location.
@@ -47,10 +14,6 @@ if ( ! function_exists( 'amazing_setup' ) ) :
 			'menu-1' => esc_html__( 'Primary', 'amazing' ),
 		) );
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
 		add_theme_support( 'html5', array(
 			'search-form',
 			'comment-form',
@@ -68,11 +31,6 @@ if ( ! function_exists( 'amazing_setup' ) ) :
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
 		add_theme_support( 'custom-logo', array(
 			'height'      => 250,
 			'width'       => 250,
@@ -83,26 +41,12 @@ if ( ! function_exists( 'amazing_setup' ) ) :
 endif;
 add_action( 'after_setup_theme', 'amazing_setup' );
 
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
+
 function amazing_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
-	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
 	$GLOBALS['content_width'] = apply_filters( 'amazing_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'amazing_content_width', 0 );
 
-/**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
 function amazing_widgets_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'amazing' ),
@@ -122,40 +66,91 @@ add_action( 'widgets_init', 'amazing_widgets_init' );
 function amazing_scripts() {
 	wp_enqueue_style( 'amazing-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'amazing-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
-	wp_enqueue_script( 'amazing-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'amazing_scripts' );
 
-/**
- * Implement the Custom Header feature.
- */
+
 require get_template_directory() . '/inc/custom-header.php';
-
-/**
- * Custom template tags for this theme.
- */
 require get_template_directory() . '/inc/template-tags.php';
-
-/**
- * Functions which enhance the theme by hooking into WordPress.
- */
 require get_template_directory() . '/inc/template-functions.php';
-
-/**
- * Customizer additions.
- */
 require get_template_directory() . '/inc/customizer.php';
 
-/**
- * Load Jetpack compatibility file.
- */
 if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+function amazing_section_title_shortcode($atts) {
+	extract(shortcode_atts( array(
+		'title' => '',
+		'description' => ''
+	), $atts ));
+
+	$amazing_section_title_markup = '';
+
+	if(!empty($title)) {
+		$amazing_section_title_markup = "<h2>$title</h2>";
+	}
+
+	if(!empty($description)) {
+		$amazing_section_title_markup .= "<p>$description</p>";
+	}
+
+	return $amazing_section_title_markup;
+}
+
+add_shortcode( 'amazing_section_title', 'amazing_section_title_shortcode' );
+
+ 
+add_action('init', 'your_init', 99 );
+ 
+function your_init() {
+ 
+	if (function_exists('kc_add_map')) { 
+	    kc_add_map(
+	        array(
+
+	            'kc_icon' => array(
+	                'name' => 'Mehedi',
+	                'description' => __('Display single icon', 'KingComposer'),
+	                'icon' => 'sl-paper-plane',
+	                'category' => 'Custom Components',
+	                'params' => array(
+	                    array(
+	                        'name' => 'icon',
+	                        'label' => 'Select Icon',
+	                        'type' => 'icon_picker',
+	                        'admin_label' => true,
+	                    ),
+	                    array(
+	                        'name' => 'icon_size',
+	                        'label' => 'Icon Size',
+	                        'type' => 'text',
+	                        'admin_label' => true,
+	                        'description' => __('Enter font-size for icon such as: 15px, 1em ..etc..', 'KingComposer')
+	                    ),
+	                    array(
+	                        'name' => 'icon_color',
+	                        'label' => 'Icon Color',
+	                        'type' => 'color_picker',
+	                        'admin_label' => true,
+	                        'description' => __('Set color for icon', 'KingComposer')
+	                    )
+	                )
+	            ),  // End of elemnt kc_icon 
+
+	        )
+	    ); // End add map
+
+
+	    add_shortcode( 'kc_icon', 'kc_icon_php_func' );
+
+	    function kc_icon_php_func() {
+	    	echo 'blah blah';
+	    }
+	
+	} // End if
+
+}  
