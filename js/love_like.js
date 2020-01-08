1fdcdd58663aad756fd79c34aa6139d86fbdea49
Coder-Mehedi <mehedi5051@gmail.com>
2020-01-08 13:51:39 +0600

    cleanup get like, set like function

--- js/love_like.js
+++ js/love_like.js
@@ -1,5 +1,11 @@
+// from single.php
 const { template_path, post_id, user_id, permalink } = post_info
 
+// DOM QUERY
+const heart_container = document.querySelector('.heart-container')
+// const result = document.getElementById('result')
+
+// Cookie Setter Function
 const setCookie = (cookie_name, cookie_value, expire_in_days) => {
         let d = new Date();
         d.setTime(d.getTime() + (expire_in_days * 24 * 60 * 60 * 1000));
@@ -7,6 +13,7 @@ const setCookie = (cookie_name, cookie_value, expire_in_days) => {
         document.cookie = cookie_name  + "=" + cookie_value + ";" + expires + ";path=/";
     }
 
+// Cookie Getter Function
 const getCookie = (cookie_name) => {
     var name = cookie_name + "=";
     var decodedCookie = decodeURIComponent(document.cookie);
@@ -23,20 +30,8 @@ const getCookie = (cookie_name) => {
     return "";
 }
 
-getCookie('loved') == 'true' ? true : false
-const heart = document.querySelector('.heart')
-const heart_container = document.querySelector('.heart-container')
-const result = document.getElementById('result')
-let cookies = getCookie(post_id) == 'true' ? true : false
-
-// if(cookies){
-//     heart_container.innerHTML = `<img class="red-heart" src="${template_path}/image/red_heart.png" alt="">`
-// }else {
-//     heart_container.innerHTML = `<li class="heart"><a href="${permalink}#liked"></a></li>`
-// }
-console.log(user_id)
-if(user_id === '0'){
-    
+// Logged Out User Function
+const getPostLikeLoggedOutUser = () => {
     if(cookies){
         heart_container.innerHTML = `<img class="red-heart" src="${template_path}/image/red_heart.png" alt="">`
         setCookie(post_id, true, 1)
@@ -50,82 +45,87 @@ if(user_id === '0'){
         const newCookie = {id: post_id, loved: false}
         console.log(newCookie)
     }
+}
 
+const setPostLikeLoggedOutUser = () => {
+    cookies = !cookies
+       
+    if(cookies){
+        heart_container.innerHTML = `<img class="red-heart" src="${template_path}/image/red_heart.png" alt="">`
+        setCookie(post_id, true, 1)
+
+        const newCookie = {id: post_id, loved: true}
+        console.log(newCookie)
+    }else {
+        heart_container.innerHTML = `<li class="heart"><a href="${permalink}#liked"></a></li>`
+        setCookie(post_id, false, 1)
+
+        const newCookie = {id: post_id, loved: false}
+        console.log(newCookie)
+    }
+}
+
+// Logged In User Function
+const getPostLikeLoggedInUser = () => {
+    jQuery.post(
+        template_path+'/inc/post_like.php',  // URL
+        { post_id , user_id, funcName: 'get_post_like'  }, // data to be submit
+        function(data, status, jqXHR) {// success callback
+            // console.log(data)
+
+            if(data){
+                heart_container.innerHTML = `<img class="red-heart" src="${template_path}/image/red_heart.png" alt="">`
+            }else {
+                heart_container.innerHTML = `<li class="heart"><a href="${permalink}#liked"></a></li>`
+            }
+
+            // result.append('status: ' + status + ', data: ' + data);
+        })
+        .done(() => console.log('request done'))
+        .fail((jqxhr, settings, ex) => alert('failed, ' + ex));
+}
+
+const setPostLikeLoggedInUser = () => {
+    jQuery.post(
+        template_path+'/inc/post_like.php',  // URL
+        { post_id , user_id, funcName: 'set_post_like' }, // data to be submit
+       function(data, status, jqXHR) {// success callback
+
+            if(data){
+                heart_container.innerHTML = `<img class="red-heart" src="${template_path}/image/red_heart.png" alt="">`
+            }else {
+                heart_container.innerHTML = `<li class="heart"><a href="${permalink}#liked"></a></li>`
+            }
+
+            // result.append('status: ' + status + ', data: ' + data);
+        }).done(() => console.log('request done'))
+          .fail((jqxhr, settings, ex) => alert('failed, ' + ex));
+}
+
+
+
+// String to Boolean Conversion
+// getCookie('loved') == 'true' ? true : false
+
+let cookies = getCookie(post_id) == 'true' ? true : false
+
+// If User Not Logged In then using Cookie
+if(user_id === '0'){
+    
+    getPostLikeLoggedOutUser();
 
     heart_container.addEventListener('click', () => {
-        cookies = !cookies
-       
-       
-
-        if(cookies){
-            heart_container.innerHTML = `<img class="red-heart" src="${template_path}/image/red_heart.png" alt="">`
-            setCookie(post_id, true, 1)
-
-            const newCookie = {id: post_id, loved: true}
-            console.log(newCookie)
-        }else {
-            heart_container.innerHTML = `<li class="heart"><a href="${permalink}#liked"></a></li>`
-            setCookie(post_id, false, 1)
-
-            const newCookie = {id: post_id, loved: false}
-            console.log(newCookie)
-        }
+        setPostLikeLoggedOutUser()
     })
 }
 
 
 
+// If User Is Logged In then Fetching From Database If The User Current Post Liked or Not
 if(user_id !== '0'){
-    jQuery.post(
-        template_path+'/inc/post_like.php',  // URL
-        { post_id , user_id, funcName: 'get_post_like'  }, // data to be submit
-        function(data, status, jqXHR) {// success callback
-            console.log(data)
-
-            if(data){
-                heart_container.innerHTML = `<img class="red-heart" src="${template_path}/image/red_heart.png" alt="">`
-                setCookie(post_id, true, 1)
-
-                const newCookie = {id: post_id, loved: true}
-                console.log(newCookie)
-            }else {
-                heart_container.innerHTML = `<li class="heart"><a href="${permalink}#liked"></a></li>`
-                setCookie(post_id, false, 1)
-
-                const newCookie = {id: post_id, loved: false}
-                console.log(newCookie)
-            }
-
-            result.append('status: ' + status + ', data: ' + data);
-        })
-        .done(() => console.log('request done'))
-        .fail((jqxhr, settings, ex) => alert('failed, ' + ex));
-
+    getPostLikeLoggedInUser();
 
     heart_container.addEventListener('click', () => {
-        cookies = !cookies
-       
-        jQuery.post(
-            template_path+'/inc/post_like.php',  // URL
-            { post_id , user_id, funcName: 'set_post_like' }, // data to be submit
-           function(data, status, jqXHR) {// success callback
-
-                if(data){
-                    heart_container.innerHTML = `<img class="red-heart" src="${template_path}/image/red_heart.png" alt="">`
-                    setCookie(post_id, true, 1)
-
-                    const newCookie = {id: post_id, loved: true}
-                    console.log(newCookie)
-                }else {
-                    heart_container.innerHTML = `<li class="heart"><a href="${permalink}#liked"></a></li>`
-                    setCookie(post_id, false, 1)
-
-                    const newCookie = {id: post_id, loved: false}
-                    console.log(newCookie)
-                }
-
-                result.append('status: ' + status + ', data: ' + data);
-            }).done(() => console.log('request done'))
-              .fail((jqxhr, settings, ex) => alert('failed, ' + ex));
+       setPostLikeLoggedInUser();
     })
 }
